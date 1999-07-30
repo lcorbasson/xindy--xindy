f9b3b19d52dcbd151ab368ffd2cd78cea117bd44
kehr <kehr>
1999-07-30 08:35:38 +0000

    Lifted length of STRING_MAX to 4096. Caused bugs in Linux as reported by T. Henlich.

--- rx/mkind.h
+++ rx/mkind.h
@@ -12,6 +12,10 @@
 #ifndef _MKIND_H
 #define _MKIND_H
 
+#ifndef PROTO
+#define PROTO( x ) x
+#endif
+
 #if    ANSI_PROTOTYPES
 #define ARGS(arg_list)	arg_list
 #define VOIDP		void*
@@ -29,11 +33,13 @@
 #undef FALSE
 #define FALSE 0
 
-#ifndef LONG_MAX
-#define LONG_MAX      1024
-#endif
 #ifndef STRING_MAX
-#define STRING_MAX    128
+/* T. Henlich reported a bug that was caused by the 128Byte limit
+   here, which was too small for his applications. Stupid thing,
+   really took me 1.5 hours to find it ;-(
+*/
+
+#define STRING_MAX    1024   /* 128 */
 #endif
 
 #define BSH '\\'
@@ -45,22 +51,14 @@ extern VOIDP fmalloc ARGS((SIZE_T size));
 
 #endif /* _MKIND_H */
 
-#ifdef __STDC__
-#include    <stddef.h>                 /* for function declarations */
-#ifndef __GNUC__
-#include    <stdlib.h>                 /* for function declarations */
-#endif
-#include    <string.h>                 /* for function declarations */
-#undef ANSI_PROTOTYPES
-#define ANSI_PROTOTYPES    1           /* so we get full argument
-                                          type checking */
-#undef SIZE_T
-#define SIZE_T  size_t
-#endif                                  /* __STDC__ */
 
 /*
   $Log$
-  Revision 1.1  1996/03/27 20:29:05  kehr
+  Revision 1.2  1999/07/30 08:35:38  kehr
+  Lifted length of STRING_MAX to 4096. Caused bugs in Linux as reported
+  by T. Henlich.
+
+  Revision 1.1  1996/03/27  20:29:05  kehr
   It works. Today I had my first success in getting the FFI running with
   the ordrules-library. The interface is defined in `ordrulei.lsp' and
   allows direct access to the functions in `ordrules.c'.
